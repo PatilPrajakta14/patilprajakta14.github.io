e257bfa4f4da180a609d51bf2daf5f4bc6a2bc36
Prajakta Patil <pprajakta1406@gmail.com>
2025-04-18 01:50:38 -0400

    Updates

---
+++ precache-manifest.3800ae71c1fe868bc33066c92c8c73bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ba21d66c5cabf8622eb173b50b67fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a6fac66195593790b1c",
+    "url": "/static/css/main.b1e77f09.chunk.css"
+  },
+  {
+    "revision": "04314c26248af0bbb390",
+    "url": "/static/js/2.6e44bbd8.chunk.js"
+  },
+  {
+    "revision": "7a6fac66195593790b1c",
+    "url": "/static/js/main.0e1dcf11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cda8bd89ee910994ad9507f979af12e2",
+    "url": "/static/media/Prajakta.cda8bd89.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/prajaktaportfolio/precache-manifest.4ee53af94f5297313680a5744e8fe313.js"
+  "/precache-manifest.3800ae71c1fe868bc33066c92c8c73bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prajaktaportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
